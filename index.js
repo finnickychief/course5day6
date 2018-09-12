3710cba40aa0ca0a5acb05356843c57452798230
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-09-12 11:41:37 -0400

    filter exercise

--- index.js
+++ index.js
@@ -43,8 +43,91 @@
         });
 
 
+    Array Methods:
+      ES6 array methods have a structure that they all share
+      
+      Each one of them passes a few parameters into their callbacks
+        1: Current item - If a string/number you can freely modify it without worrying about affecting the original array. If an object/array, it follows the same rules as a normal function
+        2: Index
+        3: Entire array(not a copy, the original array)
 
-    Array Methods
-
+      Common array methods:
+        Map - Returns a new array of the same length as the array that called it
+        Filter - Returns a new array of the same or less length as the array that called it
+        Reduce - Returns exactly 1 item
 
 */
+
+// Map
+
+// Find the square root of each item in the array
+let squareAry = [1, 4, 9, 16, 25, 36, 49];
+
+function findSquareRoot(square) {
+  return Math.sqrt(square);
+}
+
+let rootAry = squareAry.map(square => Math.sqrt(square)); // Passes in an anonymous function
+let rootAry = squareAry.map(findSquareRoot); // Passes in the declared function above
+
+console.log(rootAry);
+
+// Recreating the map method
+function ourMap(ary, callback) {
+  // Create the new array to be sent back to the caller
+  let newAry = [];
+
+  // Build the new array, using the result of the callback
+  for (let i = 0; i < ary.length; i++) {
+    // Get the result of the callback, based on the parameters we pass in. Do this for every item no matter what
+    let newItem = callback(ary[i], i, ary);
+    // Add the new item to the array
+    newAry.push(newItem);
+  }
+
+  // Return the new array
+  return newAry;
+}
+
+let rootAry = ourMap(squareAry, square => Math.sqrt(square)); // Passes in an anonymous function
+
+let secondAry = ['First', 'Second', 'Third'];
+
+let example = ourMap(secondAry, (item, i) => {
+  return `${item} is at index: ${i}`;
+});
+let example2 = secondAry.map((item, i) => `${item} is at index: ${i}`);
+
+/*
+  Filter:
+    Same structure as map with the callback as the parameter
+
+    Returns a new array of unchanged items, but chooses/filters out some of the items based on the result of the callback.
+    If the callback for an item results as truthy, we add it to our new array
+    If the callback for an item results as falsy, we skip that item and do not add it to our new array
+
+*/
+
+let numAry = [1, 3, 6, 2, 6, 8, 3, 13];
+// Find even numbers
+// if(num % 2 === 0) // If a number is divisible by 2(even)
+let evenNumbers = numAry.filter(num => num % 2 === 0);
+
+// Find odd numbers
+// if(num % 2 === 1)
+// if(num % 2 !== 0)
+// if(!(num % 2))
+let oddNumbers = numAry.filter(num => num % 2 === 1);
+
+// Find short words (words less than 7 characters)
+let wordAry = [
+  'Paradoxical',
+  'Hypotenuse',
+  'Hello',
+  'Supercalifragilisticespialidocious',
+  'Hi',
+  'Number'
+];
+
+// if(word.length < 7)
+let shortWords = wordAry.filter(word => word.length < 7);
